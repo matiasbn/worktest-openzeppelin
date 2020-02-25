b2f940d84006494fd18626e0df46ef418be39741
Matías Barrios <matiasbn@MacBook-Air-de-Matias.local>
2020-02-25 16:29:50 -0300

    feat: contracts commented

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -10,22 +10,29 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
  * @notice Fully-compliant ERC20 mintable token
  */
 contract Token is ERC20Mintable {
-
     string private constant _name = "Token";
     string private constant _symbol = "TKN";
     uint8 private constant _decimals = 18;
 
     constructor() public {
         uint8 base = 10;
-        uint256 initialSupply = 1000 * (base ** _decimals);
+        uint256 initialSupply = 1000 * (base**_decimals);
         // The account creating the token receives the whole initial supply
         _mint(msg.sender, initialSupply);
     }
 
-    function timelockTransfer(address beneficiary, uint amount, uint releaseTime) external {
+    function timelockTransfer(
+        address beneficiary,
+        uint256 amount,
+        uint256 releaseTime
+    ) external {
         // Deploy timelock contract
-        TokenTimelock newTimelock = new TokenTimelock(IERC20(address(this)), beneficiary, releaseTime);
-        
+        TokenTimelock newTimelock = new TokenTimelock(
+            IERC20(address(this)),
+            beneficiary,
+            releaseTime
+        );
+
         // Send tokens to timelock
         transfer(address(newTimelock), amount);
     }
@@ -33,10 +40,23 @@ contract Token is ERC20Mintable {
     /**
      * Issue new tokens, but first keep them in a timelock contract for a specific period of time
      */
-    function timelockMint(address beneficiary, uint256 amount, uint256 releaseTime) external {
+
+    function timelockMint(
+        address beneficiary,
+        uint256 amount,
+        uint256 releaseTime
+    ) external {
         // Deploy timelock contract
-        TokenTimelock newTimelock = new TokenTimelock(IERC20(address(this)), beneficiary, releaseTime);
-        
+        TokenTimelock newTimelock = new TokenTimelock(
+            IERC20(address(this)),
+            beneficiary,
+            releaseTime
+        );
+
+        /**
+        * @dev this was supposed to be the 'mint' function, not the '_mint' function. '_mint' is an internal function at the
+        ERC20.sol contract without access control i.e. anyone could mint tokens without being the minter.
+        */
         // Mint tokens to timelock
         _mint(address(newTimelock), amount);
     }
--- contracts/TokenDistributor.sol
+++ contracts/TokenDistributor.sol
@@ -17,18 +17,24 @@ contract TokenDistributor {
     address private _tokenAddress;
 
     // Array to hold all addresses that will receive a payment
+    /**
+      * @dev visibility not specified, defaults to public
+     */
     address[] beneficiaries;
 
     // Total amount of tokens to distribute
     uint256 private _totalAmount;
 
     // Mapping to keep track of how many tokens each beneficiary will receive
-    mapping (address => uint256) amountsByBeneficiary;
-    
+    /**
+      * @dev visibility not specified, defaults to public
+     */
+    mapping(address => uint256) amountsByBeneficiary;
+
     modifier onlyOwner() {
         require(msg.sender == _owner);
         _;
-    }   
+    }
 
     /**
      * Constructor
@@ -42,9 +48,19 @@ contract TokenDistributor {
     /**
      * Add the address of a beneficiary that will receive a certain amount of tokens.
      */
-    function registerBeneficiary(address beneficiary, uint256 amount) onlyOwner external {
+
+    /**
+      * @dev action with sensitive changes that does not emit any event.
+     */
+    function registerBeneficiary(address beneficiary, uint256 amount)
+        external
+        onlyOwner
+    {
         require(amount > 0, "Amount must be greater than zero");
 
+        /**
+         * @dev Integer overflow danger, should use SafeMath.
+         */
         _totalAmount += amount;
         beneficiaries.push(beneficiary);
         amountsByBeneficiary[beneficiary] += amount;
@@ -53,14 +69,34 @@ contract TokenDistributor {
     /**
      * Decrease, by the given amount, the number of tokens a beneficiary will receive.
      */
-    function decreaseBenefit(address beneficiary, uint256 amount) public onlyOwner {
-        require(amountsByBeneficiary[beneficiary] != 0, "Beneficiary does not exist");
-        
+
+    /**
+      * @dev action with sensitive changes that does not emit any event.
+     */
+
+    function decreaseBenefit(address beneficiary, uint256 amount)
+        public
+        onlyOwner
+    {
+        require(
+            amountsByBeneficiary[beneficiary] != 0,
+            "Beneficiary does not exist"
+        );
+
+        /**
+          * @dev Integer underflow danger, should use SafeMath.
+         */
         // Decrease total and beneficiary's amount
         _totalAmount -= amount;
         amountsByBeneficiary[beneficiary] -= amount;
     }
 
+    /**
+      * @dev Although only the owner can call this function, the size of beneficiaries can increase to the point of
+      * having a DoS with block gas limit. A withdrawal pattern is recommended in this case 
+      * (https://solidity.readthedocs.io/en/v0.6.3/common-patterns.html#withdrawal-from-contracts)
+      * @dev Should emit an event if is transferrring tokens.
+     */
     function payAllBeneficiaries() external onlyOwner {
         for (uint8 index = 0; index < beneficiaries.length; index++) {
             _paySingleBeneficiary(beneficiaries[index]);
@@ -70,13 +106,20 @@ contract TokenDistributor {
     /**
      * Private function to pay a single beneficiary
      */
-    function _paySingleBeneficiary(address beneficiary) {        
+
+    /**
+      * @dev visibility not specified, defaults to public i.e. anyone can call this function
+      */
+    function _paySingleBeneficiary(address beneficiary) {
         uint256 amount = amountsByBeneficiary[beneficiary];
 
         // Transfer tokens to beneficiary
         IERC20(_tokenAddress).transfer(beneficiary, amount);
 
         // Decrease total amount of tokens to be distributed
+        /**
+          * @dev Integer underflow danger, should use SafeMath.
+         */
         _totalAmount -= amount;
     }
 
@@ -89,15 +132,20 @@ contract TokenDistributor {
     function getAmount(address beneficiary) external view returns (uint256) {
         return amountsByBeneficiary[beneficiary];
     }
-
+    /**
+      * @dev mutability can be specified as view.  
+     */
     function owner() external returns (address) {
         return _owner;
     }
 
+    /**
+      * @dev mutability can be specified as view.  
+     */
     function tokenAddress() external returns (address) {
         return _tokenAddress;
     }
-    
+
     function totalAmount() external view returns (uint256) {
         return _totalAmount;
     }
